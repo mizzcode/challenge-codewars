0ae808ed6e75256ba5a81870d88ed10e7609f96a
Misbahudin <misbahudin.id@gmail.com>
2024-02-22 14:58:18 +0700

    create new string without vowels in new str

--- 7kyu/DisemvowelTrolls.js
+++ 7kyu/DisemvowelTrolls.js
@@ -1,5 +1,29 @@
 /**
- * Day 2/365.
+ * Day 5/365.
  * https://www.codewars.com/kata/52fba66badcd10859f00097e
- * @returns If it is a square, return its area. If it is a rectangle, return its perimeter.!
+ * For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+ * Your task is to write a function that takes a string
+ * @returns a new string with all vowels removed.
  */
+
+// function disemvowel(str) {
+//   const vowels = ['a', 'i', 'u', 'e', 'o']
+
+//   let newStr = ''
+
+//   for (let i = 0; i < str.length; i++) {
+//     const deleteVowel = vowels.includes(str.toLowerCase().at(i))
+//     if (!deleteVowel) {
+//       newStr += str.at(i)
+//     }
+//   }
+
+//   return newStr
+// }
+
+function disemvowel(str) {
+  return str.replace(/[aiueo]/gi, '')
+}
+
+console.log(disemvowel('This website is for losers LOL!'))
+console.log(disemvowel('LMAO'))
