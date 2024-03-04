40816351c4262a97eb77db07a20273bdc9ae2641
Misbahudin <misbahudin.id@gmail.com>
2024-03-04 21:53:17 +0700

    day 8

---
+++ 7kyu/StringEndsWith.js
@@ -0,0 +1,14 @@
+/**
+ * Day 8/365.
+ * https://www.codewars.com/kata/51f2d1cafc9c0f745c00037
+ * Complete the solution so that it.
+ * @returns returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+ */
+
+function solution(str, ending){
+  // TODO: complete
+  return str.endsWith(ending)
+}
+
+console.log(solution('asd', 'sd'))
+console.log(solution('mizz', 'zzz'))
